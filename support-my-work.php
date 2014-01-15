70c3d898830cfa082a567e8e7e35fd830c661f62
Jen Montes <jen@jenmontes.com>
2014-01-14 20:28:46 -0500

    Added classes to the PayPal form. Fixes issue #1

--- support-my-work.php
+++ support-my-work.php
@@ -62,25 +62,25 @@ class Support_My_Work extends WP_Widget {
 		<?php if ($instance['widget_title']): ?>
 		<h5 class="widget-title"><?php echo $instance['widget_title']; ?></h5>
 		<?php endif; ?>
-		<form id="support-my-work-form" action="https://www.paypal.com/cgi-bin/webscr" method="post" target="paypal">
-		<p><?php echo(isset($instance['input_label']) ? $instance['input_label'] : "Pay what you want:"); ?></p>
-		<input type="text" name="amount" style="margin-bottom: 5px;">
-		<input type="image" src="<?php if ($instance['custom_button'] != "") {echo $instance['custom_button'];} else {echo "https://www.paypalobjects.com/en_US/i/btn/btn_buynow_LG.gif";} ?>" border="0" name="submit">
-		<img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
-		<input type="hidden" name="cmd" value="_xclick">
-		<input type="hidden" name="business" value="<?php echo $instance['business_id']; ?>">
-		<input type="hidden" name="item_name" value="<?php echo $instance['item_name']; ?>">
-		<input type="hidden" name="item_number" value="">
-		<input type="hidden" name="no_shipping" value="1">
-		<input type="hidden" name="shipping" value="0.00">
-		<input type="hidden" name="tax" value="0.00">
-		<input type="hidden" name="button_subtype" value="services">
-		<input type="hidden" name="no_note" value="0">
-		<input type="hidden" name="cn" value="Add special instructions to the seller:">
-		<input type="hidden" name="country" value="US">
-		<input type="hidden" name="currency_code" value="USD">
-		<input type="hidden" name="lc" value="US">
-		<input type="hidden" name="bn" value="PP-BuyNowBF:btn_paynowCC_LG.gif:NonHosted">
+		<form class="support-my-work-form" action="https://www.paypal.com/cgi-bin/webscr" method="post" target="paypal">
+			<p class="support-my-work-text-label"><?php echo(isset($instance['input_label']) ? $instance['input_label'] : "Pay what you want:"); ?></p>
+			<input class="support-my-work-text-input" type="text" name="amount" style="margin-bottom: 1em;">
+			<input class="support-my-work-button" type="image" src="<?php if ($instance['custom_button'] != "") {echo $instance['custom_button'];} else {echo "https://www.paypalobjects.com/en_US/i/btn/btn_buynow_LG.gif";} ?>" border="0" name="submit">
+			<img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
+			<input type="hidden" name="cmd" value="_xclick">
+			<input type="hidden" name="business" value="<?php echo $instance['business_id']; ?>">
+			<input type="hidden" name="item_name" value="<?php echo $instance['item_name']; ?>">
+			<input type="hidden" name="item_number" value="">
+			<input type="hidden" name="no_shipping" value="1">
+			<input type="hidden" name="shipping" value="0.00">
+			<input type="hidden" name="tax" value="0.00">
+			<input type="hidden" name="button_subtype" value="services">
+			<input type="hidden" name="no_note" value="0">
+			<input type="hidden" name="cn" value="Add special instructions to the seller:">
+			<input type="hidden" name="country" value="US">
+			<input type="hidden" name="currency_code" value="USD">
+			<input type="hidden" name="lc" value="US">
+			<input type="hidden" name="bn" value="PP-BuyNowBF:btn_paynowCC_LG.gif:NonHosted">
 		</form>
 		
 		<?php
